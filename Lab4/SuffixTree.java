3d5c26a9d2e1d278bda0534c7c1e7136c7c1beb5
timendez <hikeemhigh@gmail.com>
2015-10-23 15:55:06 -0700

    save

--- Lab4/SuffixTree.java
+++ Lab4/SuffixTree.java
@@ -5,7 +5,7 @@ public class SuffixTree {
    private String sequence;
 
    public SuffixTree(String dna) {
-      root = new InternalNode(0, dna.length() - 1);
+      root = new InternalNode(-1, -1);
       sequence = dna;
       buildTree();
    }
@@ -16,11 +16,54 @@ public class SuffixTree {
 
    private void buildTree() {
       Node child = new LeafNode(1, 0, sequence.length() - 1);
+      TraverseInfo traversal;
 
       root.addChild(sequence.charAt(0), child);
 
       for(int i = 0; i < sequence.length(); i++) {
-         
+         traversal = traverse(root, null, sequence.substring(i));
+
+      }
+   }
+
+   //Returns last traveresed parent node and index of last matched char
+   private TraverseInfo traverse(Node current, Node parent, String toMatch) {
+      int j = current.startIdx, i;
+
+      if(current.startIdx != -1) {
+         for(i = 0; j < current.endIdx && i < toMatch.length; i++, j++) {
+            //If should split
+            if(toMatch.charAt(i) != sequence.charAt(j) {
+               return new TraverseInfo(j - 1, current, parent);
+            }
+         }
+      }
+
+      if(current instanceOf InternalNode) {
+         if(toMatch.charAt(i) == 'a' && current.a) {
+            traverse(current.a, toMatch.substring(i));
+         }
+      }
+
+      TraverseInfo data;
+
+      return data;
+   }
+
+   //Adding another leaf node to an preexisting leaf node
+   private void graft() {
+      
+   }
+
+   private class TraverseInfo {
+      int indexOfChar;
+      Node current;
+      Node parent;
+
+      public TraverseInfo(int idx, Node cur, Node par) {
+         indexOfChar = idx;
+         current = cur;
+         parent = par;
       }
    }
 
@@ -71,7 +114,7 @@ public class SuffixTree {
    }
 
    private abstract class Indices {
-      int startIdx;
-      int endIdx;
+      public int startIdx;
+      public int endIdx;
    }
 }
